ac833ac4087bd3f6479cba8ab5db645f467d4add
Patrick_B <pb5191@gmail.com>
2022-12-10 00:29:33 -0500

    Add Navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,17 @@ import styles from "../styles/Navbar.css"
 
 const Navbar = () => {
   return (
-    <nav className="navbar"
+    <nav className={styles.navbar}>
+      <div>
+        <Image className={ styles.navlogo} src="../public/software-logo.png" alt="logo"/>
+      </div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </nav>
   );
 }
 
