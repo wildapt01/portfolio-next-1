09675824aab2ff51b6399d506d41138274528c0d
Patrick_B <pb5191@gmail.com>
2022-12-10 00:22:32 -0500

    Add Footer component and style

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav className="navbar"
+  );
+}
+
+export default Navbar;
