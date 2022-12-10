8820a94eba570d2790c889dedb6214d408584a77
Patrick_B <pb5191@gmail.com>
2022-12-10 00:22:43 -0500

    Add Footer component and style

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Footer.css"
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      Copyright P. Boutier - 2022, 2023
+    </footer>
+  );
+}
+
+export default Footer;
