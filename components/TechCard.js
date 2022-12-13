8c968576d5066d411cda9c4e1f0ee957e23ab200
Patrick_B <pb5191@gmail.com>
2022-12-12 22:56:07 -0500

    Add page TechStack

---
+++ components/TechCard.js
@@ -0,0 +1,11 @@
+// import styles from "../styles/TechStack.module.css";
+
+const TechCard = () => {
+  return (
+    <>
+      <div>front end techs</div>
+    </>
+  );
+};
+
+export default TechCard;
--- pages/techstack/index.js
+++ pages/techstack/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from "../../styles/TechStack.module.css";
 
 const TechStack = () => {
   return (
@@ -9,6 +10,11 @@ const TechStack = () => {
       </Head>
       <div className="container">
         <h2>Technologies</h2>
+        <section className={styles.cards}>
+          <div className={styles.card}>Front End</div>
+          <div className={styles.card}>Back End</div>
+          <div className={styles.card}>Others</div>
+        </section>
       </div>
     </>
   );
